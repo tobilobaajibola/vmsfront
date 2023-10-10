20f511e647a0b0d38c86340e3d9c0b7013aff1ee
tobilobaajibola <41303448+tobilobaajibola@users.noreply.github.com>
2023-10-10 11:47:30 -0500

    Delete app/backup/application directory

--- app/backup/application/views/general/admin_foottag.php
+++
@@ -1,6 +0,0 @@
-<script src="<?php echo base_url();?>assets/admin/js/core.min.js"></script>
-
-  <!-- <script type="text/javascript" src="<?php echo base_url();?>assets/js/scripts.js"></script> -->
-<script type="text/javascript" src="<?php echo base_url();?>assets/js/customscript.js"></script>
-
- 
--- app/backup/application/views/pages/admin/boxes/list_employee.php
+++
@@ -1,5 +0,0 @@
-<?php
-foreach ($list_employee as $list_employees) {
-echo $list_employees['firstname'];	
-}
-?>
