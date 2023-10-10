085701b129d831bad3a67842b2d2339577069a1f
tobilobaajibola <41303448+tobilobaajibola@users.noreply.github.com>
2023-10-10 12:02:31 -0500

    Delete app directory

--- app/push/config.js
+++
@@ -1,4 +0,0 @@
-window.GoogleSamples = window.GoogleSamples || {};
-window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: 'AAAA9ZZ8Vlc:APA91bFt8J-Y3OlF7s7RvLbj7TGWT4Uh-jcE7_ZR38WcNrUpWprSssFD_FpcOtJoKuHgvX6eWkrJS3EUkjG4yzCifF8u85kHqwQEo7YC97MOKYmZpXd6-2nYhFluMq7yUNiduqsm0e3D'
-};
